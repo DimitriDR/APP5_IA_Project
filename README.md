5c15d7a04f0cdc466dad9a21c047f9dc0932c76e
DimitriDR <dimitridroeck@gmail.com>
2024-01-02 21:11:37 +0100

    Continuing the main algorithm with vectorization working. Adding README.md.

---
+++ README.md
@@ -0,0 +1,33 @@
+This project is simply, a free subject, academic project
+where we are asked to manipulate Machine Learning.
+
+The goal of this project is to create a program
+that can make kind of CV screening.
+
+# Table of contents
+- [Installation](#installation)
+  - [Requirements](#requirements)
+  - [Install dependencies with requirements.txt](#install-dependencies-with-requirementstxt)
+
+# Installation
+## Requirements
+- Python 3.12 (may work with older versions)
+- pip
+
+## Install dependencies with requirements.txt
+```bash
+pip install -r requirements.txt
+```
+
+It is possible that punkt is not installed, if so, run:
+```bash
+python
+nltk.download('punkt')
+```
+
+
+# Usage
+## Run the program
+```bash
+python main.py
+```
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import ssl
+
 import nltk
+import numpy as np
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.model_selection import train_test_split
 
 
 def cleaning_data(data_frame: pd.DataFrame) -> pd.DataFrame:
@@ -19,6 +25,18 @@ def cleaning_data(data_frame: pd.DataFrame) -> pd.DataFrame:
     data_frame["Resume_str"] = data_frame["Resume_str"].apply(lambda x: x.lower())
 
     return data_frame
+
+
+def data_vectorization(data_frame: pd.DataFrame):
+    """
+    Function to vectorize the data frame (dataset) to return only what I need.
+    :param data_frame: The data frame (dataset) to vectorize
+    :return: The vectorized data frame (dataset)
+    """
+    vectorizer = TfidfVectorizer(stop_words="english", max_features=1000)
+    processed_data_frame = vectorizer.fit_transform(data_frame["Resume_str"].apply(lambda x: np.str_(x)))
+
+    return processed_data_frame, vectorizer
 
 
 def process_data(data_frame: pd.DataFrame) -> pd.DataFrame:
@@ -46,11 +64,14 @@ def tokenize_data(data_frame: pd.DataFrame) -> pd.DataFrame:
 
 
 def main():
-    # We need, punkt to tokenize the text, so we check if it's already downloaded
     try:
-        nltk.data.find("tokenizers/punkt")
-    except LookupError:
-        nltk.download("punkt")
+        _create_unverified_https_context = ssl._create_unverified_context
+    except AttributeError:
+        pass
+    else:
+        ssl._create_default_https_context = _create_unverified_https_context
+
+    nltk.download()
 
     # Variable containing the path to PDF CVs we want to analyze
     # folder_to_analyze: str = "./dataset/data/DIGITAL-MEDIA"
@@ -59,17 +80,31 @@ def main():
     data_frame: pd.DataFrame = pd.read_csv("dataset/resumes/resumes.csv")
 
     data_frame: pd.DataFrame = cleaning_data(data_frame)
+
+    Y = data_frame["Category"]
+
     data_frame: pd.DataFrame = process_data(data_frame)
+
     data_frame = tokenize_data(data_frame)
 
-    # We join the tokens to get the resume as a string
-    data_frame["Resume_str"] = data_frame["Resume_str"].apply(lambda x: ' '.join(x))
+    # We vectorize the data frame to make it easier for the machine learning algorithm
+    X, vectorizer = data_vectorization(data_frame)
 
-    # We initialize the TF-IDF vectorizer to convert the text into a matrix of TF-IDF features
-    tfidf_vectorizer = TfidfVectorizer()
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
 
-    # We fit and transform the data frame (dataset) to get the TF-IDF features
-    tfidf_matrix = tfidf_vectorizer.fit_transform(data_frame["Resume_str"])
+    # Train a Random Forest Classifier
+    clf = RandomForestClassifier(random_state=42)
+    clf.fit(X_train, y_train)
+
+    # Predict on the test set
+    y_pred = clf.predict(X_test)
+
+    # Print the classification report and confusion matrix
+    print(classification_report(y_test, y_pred))
+    print(confusion_matrix(y_test, y_pred))
+
+    #    print(processed_data_frame)
+    print(vectorizer)
 
 
 if __name__ == '__main__':
